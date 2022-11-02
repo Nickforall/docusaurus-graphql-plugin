5fcd6e36a75ce39fbd23d45a63e3cfdbd848cd2f
Nick Vernij <hello@nickforall.nl>
2022-11-02 14:42:02 +0800

    Add build

--- packages/docusaurus-graphql-plugin/build/converters/parseMarkdown.js
+++ packages/docusaurus-graphql-plugin/build/converters/parseMarkdown.js
@@ -7,11 +7,14 @@ exports.parseMarkdown = void 0;
 const marked_1 = __importDefault(require("marked"));
 function parseMarkdown(markdown) {
     const walkTokens = (token) => {
-        if (token.type === "text" || token.type == "code") {
-            // make the Markdown compatible with MDX by escaping curly braces
+        // make the Markdown compatible with MDX by escaping curly braces
+        if (token.type === "text") {
             token.text = token.text
                 .replace(/\{/g, "&lbrace;")
                 .replace(/\}/g, "&rbrace;");
+        }
+        if (token.type === "codespan" || token.type === "code") {
+            token.text = token.text.replace(/\{/g, "&#123;").replace(/\}/g, "&#125;");
         }
     };
     return marked_1.default
